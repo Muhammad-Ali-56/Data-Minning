1c74038b3e0a4d99139a582d07fc6d7ae6c55978
Muhammad Ali <139348601+Muhammad-Ali-56@users.noreply.github.com>
2023-08-20 13:39:14 +0500

    Data Mining Project.sql

--- Data Mining Project.sql
+++ Data Mining Project.sql
@@ -1,25 +1,25 @@
 use mining_data;
-#					Query 1:  Meta Data
+#				Query 1:  Meta Data
 show tables;
 select project_data;
 select * from project_data
 limit 15;
 
-#  					Query 2: What is the distribution of order values across all customers in the dataset?
+#  				Query 2: What is the distribution of order values across all customers in the dataset?
 select CustomerID, sum(Quantity * UnitPrice) as Total_Order_Value
 from project_data
 group by CustomerID
 order by Total_Order_Value desc
 limit 15;
 
-#						Query 3: How many unique products has each customer purchased?
+#				Query 3: How many unique products has each customer purchased?
 select CustomerID, count(distinct StockCode) as Unique_Product_Count
 from project_data
 group by CustomerID
 order by 2 desc
 limit 15;
 
-#					Query 4: Which customers have only made a single purchase from the company?
+#				Query 4: Which customers have only made a single purchase from the company?
 select CustomerID, count(distinct InvoiceNo) as Number_Of_Purchases
 from project_data
 group by CustomerID
@@ -35,9 +35,9 @@ group by Product_1, Product_2, InvoiceNO, CustomerID
 order by Frequency desc
 limit 10;
 
-# 												ADVANCE QUEIRIES
+# 							ADVANCE QUEIRIES
 
-#					 1.      Customer Segmentation by Purchase Frequency
+#				        1.      Customer Segmentation by Purchase Frequency
 select CustomerID,
        case
           when count(distinct InvoiceDate) > 10 then 'High'
@@ -49,19 +49,19 @@ group by CustomerID
 order by Purchase_Frequency desc
 limit 15;
 
-#						 2. Average Order Value by Country
+#					 2. Average Order Value by Country
 select Country, avg(Quantity * UnitPrice) as Avarage_Order_Value
 from project_data
 group by Country;
 
-#									 3. Customer Churn Analysis
+#					 3. Customer Churn Analysis
 select CustomerID
 from project_data
 where InvoiceDate <= date_sub(now(), interval 6 month)
 group by CustomerID
 limit 15;
 
-# 				4. Product Affinity Analysis
+# 				         4. Product Affinity Analysis
 select t1.CustomerID, t1.InvoiceNo, t1.StockCode as Product_1, t2.StockCode as Product_2, count(*) as Frequency
 from project_data as t1
 join project_data as t2 on t1.InvoiceNo=t2.InvoiceNo and t1.StockCode < t2.StockCode
